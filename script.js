93ad2717af5a437501b4955ad5202606550d4406
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-27 10:38:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+        let nome = prompt("Digite seu nome:");
+        let cidade = prompt("Digite sua cidade:");
+        let curso = prompt("Digite seu curso:");
+
+        alert("Olá " + nome + "! Você é de " + cidade + " e está cursando " + curso + ".");
+ 
