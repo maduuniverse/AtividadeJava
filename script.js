c9eb942fe93ea86a4e5236aec6f3ba07480aca0c
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-27 10:31:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+        let palavra1 = prompt("Escolha uma palavra:");
+        let palavra2 = prompt("Escolha outra palavra:");
+    
+        alert("Você escolheu as palavras " + palavra1 + " e " + palavra2);
