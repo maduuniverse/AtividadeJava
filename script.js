fa0f89c9df894009eb0034489cd9a403f2e8f126
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-27 10:14:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+        let numero1 = Number (prompt("Digite um número:"));
+        let numero2 = Number (prompt("Digite outro número:"));
+
+        let soma = numero1 + numero2
+        alert("A soma dos números " + numero1 + " e " + numero2 + " é: " + soma);
+        alert(`A soma dos números ${numero1} e ${numero2} é: ${soma}`);
+
+
