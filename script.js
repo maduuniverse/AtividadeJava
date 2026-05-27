4db6e7108c1ef4092d9f9106ddcc57506baefd44
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-27 10:23:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+        let pizza = Number(prompt("Quantos pedaços de pizza você comeu?"));
+        let copo = Number(prompt("Quantos copos de refrigerante você bebeu?"));
+
+        alert("Você comeu " + pizza + " pedaços de pizza e bebeu " + copo + " copos de refrigerante.");
